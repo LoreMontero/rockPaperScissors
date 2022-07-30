044c5e35e0342f4ac1d559a25d699247f9919120
Lorenzo Montero <lorenzomontero@Lorenzos-MacBook-Air.local>
2022-07-30 16:26:08 -0400

    Added computer randomization option

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const getComputerChoice = () => {
-    
+const selections = [
+    {
+        name: "rock",
+        beats: "scissors"
+    },
+    {
+        name: "paper",
+        beats: "rock"
+    },
+    {
+        name: "scissors",
+        beats: "paper"
+    }
+]
+
+const computerChoice = () => {
+    const randomSelection = Math.floor(Math.random() * selections.length)
+    return selections[randomSelection];
 }
+
+const computerSelection = computerChoice();
+console.log(computerSelection);
